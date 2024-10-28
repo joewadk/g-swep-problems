181e8a1dae7963f98655969f9c52c3568de07a1b
joewadk <jkabir000@citymail.cuny.edu>
2024-10-28 14:41:32 -0400

    added solution

--- oa_problem.py
+++ oa_problem.py
@@ -17,7 +17,7 @@ Output:
 Example:
 
 laps = [["Harold 160", "Gina 155", "Juan 160"],
-        ["Sam 152", "Gina 155", "Harold 160"],
+        ["Juan 152", "Gina 155", "Harold 160"],
         ["Gina 150", "Juan 151"]]
 
 Output:
@@ -30,7 +30,7 @@ Explanation:
   
 - Second lap: 
     - Gina again has the fastest time of 155 seconds.
-    - Harold has the slowest time of 160 seconds, but he was already eliminated, so we don't consider him. Sam has 152 seconds and survives this lap.
+    - Harold has the slowest time of 160 seconds, but he was already eliminated, so we don't consider him. Juan has 152 seconds and survives this lap.
   
 - Third lap: 
     - Gina survives with a time of 150 seconds.
@@ -43,3 +43,61 @@ Constraints:
 2. Driver names are unique.
 3. Times are always positive integers.
 """
+
+'''
+my thoughts:
+
+assume drivers are unique
+use the split operator to separate the driver name and time
+use the max function on times to get the slowest driver
+use a set to store disqualified drivers
+
+
+handle the case where duplicate drivers are eliminated in the same lap, sorted alphabetically
+base case: no drivers in the lap, move to next lap
+stopping condition: after all rows (laps) have been iterated over, then return result
+
+
+some considerations
+how do we handle the case where all drivers are eliminated in the same lap?
+
+
+
+'''
+
+
+
+def solution(laps):
+    all_seen = set()  #using a set to track disqualified drivers. if there is a tie and we try to extend with a duplicate driver already in the set, because sets hold only uniques the duplicate driver is skipped and we only add the non-duplicate driver.
+    #we use a set here as opposed to a list so that we can check if the driver is already disqualified
+    #based on some research, using the not in operator on sets has O(1) time as it's built like a hash table, but using not in operator on arrays we get O(n) because we check all items 
+    
+    result = [] #output array
+    
+    for lap in laps: #iterate through rows
+        current_drivers = [d for d in lap if d.split()[0] not in all_seen] #list of drivers who are not disqualified. in the last lap once all drivers are disqualified, only 1 remains and so naturally that one driver has the slowest time and gets added to the end of the result list.
+        
+        if not current_drivers:  #if we get an empty list, move to next lap
+            break
+        
+        slowest_time = max(int(d.split()[1]) for d in current_drivers) #using max to get the slowest time, int typecast to convert from string
+        slowest_drivers = [d.split()[0] for d in current_drivers if int(d.split()[1]) == slowest_time]
+        
+        slowest_drivers.sort() #sort any disqualified drivers alphabetically before adding them to the set, this ensures double disqualified drivers are sorted
+        
+        result.extend(slowest_drivers) #using extend instead of append to add multiple drivers at once in the case of a double disqualify
+        all_seen.update(slowest_drivers) #set built in to update the set with the slowest drivers. if a driver in slowest driver already in all_seen then it skips over
+    
+    return result
+
+
+#example from the screenshot to verify the solution
+laps = [
+    ["Harold 154", "Gina 155", "Juan 160"],
+    ["Juan 152", "Gina 155", "Harold 160"],
+    ["Harold 148","Gina 150", "Juan 151"]
+]
+out = solution(laps)
+print(out)  #we should get "juan, harold, gina"
+
+#screenshot doesnt have other cases to test unfortuntately.
